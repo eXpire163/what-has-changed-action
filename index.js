b90558cc6c223895ed94b511f2f23ebb8c8360bf
eXpire163 <github@mms2k.de>
2021-11-15 11:01:57 +0100

    update

--- index.js
+++ index.js
@@ -70,7 +70,14 @@ function validateDiff(delta, filename) {
 
 function setResult(filename, result, reason) {
   summery.set(filename, { result: result, reason: reason })
-  console.log(`File ${filename} was ${reason} ${result ? "✔" : "✖"}`)
+}
+function printSummery() {
+
+  console.log("########### result ##########");
+
+  summery.forEach((key, value) => {
+    console.log(`File ${key} was ${value.reason} ${value.result ? "✔" : "✖"}`)
+  });
 }
 
 
@@ -86,104 +93,108 @@ async function run() {
     const context = github.context;
 
 
-    if (context.eventName == "pull_request") {
-      //getting pr related information
-      const payload = context.payload
-      const repository = payload.repository
+    if (context.eventName != "pull_request") {
+      console.log("this pipeline is only for pull requests")
+      return
+    }
+    //getting pr related information
+    const payload = context.payload
+    const repository = payload.repository
 
-      org = repository.owner.login
-      repo = repository.name
-      pull_number = payload.number
-      filesChanged = payload.pull_request.changed_files
+    org = repository.owner.login
+    repo = repository.name
+    pull_number = payload.number
+    filesChanged = payload.pull_request.changed_files
 
-      console.log("ℹ this is a pr", repository.owner.login,
-        repository.name,
-        payload.number)
-      //load pr files
-      const thisPR = await octokit.rest.pulls.listFiles({
-        owner: org,
-        repo: repo,
-        pull_number: pull_number
-      });
-      const files = thisPR.data
+    console.log("ℹ this is a pr", repository.owner.login,
+      repository.name,
+      payload.number)
+    //load pr files
+    const thisPR = await octokit.rest.pulls.listFiles({
+      owner: org,
+      repo: repo,
+      pull_number: pull_number
+    });
+    const files = thisPR.data
 
-      //iterating over changed files
-      summery = new Map();
-      for (const file of files) {
+    //iterating over changed files
+    summery = new Map();
+    for (const file of files) {
 
-        filename = file.filename
+      filename = file.filename
 
-        // create or delete can not be merged automatically
-        if (file.status != "modified") {
-          setResult(filename, false, "file is new or deleted")
-          continue
-        }
-
-        //only allowing yaml/yml files
-        if (filename.endsWith(".yaml") || filename.endsWith(".yml"))
-          console.log("ℹ file is a yml/yaml")
-        else {
-          setResult(filename, false, "file is not a yaml")
-          continue
-        }
-
-        //check for noCheckFiles (whitelist)
-
-        //ignore the first x folders in the path - like project name that could change
-        //techdebt - make it smarter
-        simplePath = filename
-        for (let i = 0; i < 2; i++) {
-          simplePath = simplePath.substring(simplePath.indexOf('/') + 1)
-        }
-
-        if (options.noCheckFiles.includes(simplePath)) {
-          setResult(filename, true, "part of noCheckFiles")
-          continue
-        }
-
-
-        // compare content of yaml files
-
-        //get master
-        contentRequest = { owner: org, repo: repo, path: filename }
-        jsonOld = await getContent(contentRequest, octokit)
-
-        //get current
-        contentRequest = { owner: org, repo: repo, path: filename, ref: payload.pull_request.head.ref }
-        jsonNew = await getContent(contentRequest, octokit)
-
-        //check if both have valid content
-        if (jsonOld == null || jsonNew == null) {
-          setResult(filename, false, "could not read file content")
-        }
-
-        // run the compare
-        var delta = diffPatcher.diff(jsonOld, jsonNew);
-        console.log("ℹ delta", delta)
-        //console.log(jsonDiffPatch.formatters.console.format(delta))
-
-
-        result = validateDiff(delta, simplePath)
-        setResult(filename, result.result, result.msg)
-
-      }
-      console.log("########### result ##########");
-      console.log(summery)
-      if (summery.size != filesChanged) {
-        throw ("Some files could not be classified, should be / was", filesChanged, summery.size)
+      // create or delete can not be merged automatically
+      if (file.status != "modified") {
+        setResult(filename, false, "file is new or deleted")
+        continue
       }
 
-      console.log("All files could be classified ✔")
-      //check if map contains "false" elements
-      falseMap = new Map([...summery].filter(([k, v]) => v.result == false))
-      if (falseMap.size > 0) {
-        console.log("cannot allow auto merge")
-      }
+      //only allowing yaml/yml files
+      if (filename.endsWith(".yaml") || filename.endsWith(".yml"))
+        console.log("ℹ file is a yml/yaml")
       else {
-        console.log("all files seem to be valid and can be merged")
+        setResult(filename, false, "file is not a yaml")
+        continue
       }
+
+      //check for noCheckFiles (whitelist)
+
+      //ignore the first x folders in the path - like project name that could change
+      //techdebt - make it smarter
+      simplePath = filename
+      for (let i = 0; i < 2; i++) {
+        simplePath = simplePath.substring(simplePath.indexOf('/') + 1)
+      }
+
+      if (options.noCheckFiles.includes(simplePath)) {
+        setResult(filename, true, "part of noCheckFiles")
+        continue
+      }
+
+
+      // compare content of yaml files
+
+      //get master
+      contentRequest = { owner: org, repo: repo, path: filename }
+      jsonOld = await getContent(contentRequest, octokit)
+
+      //get current
+      contentRequest = { owner: org, repo: repo, path: filename, ref: payload.pull_request.head.ref }
+      jsonNew = await getContent(contentRequest, octokit)
+
+      //check if both have valid content
+      if (jsonOld == null || jsonNew == null) {
+        setResult(filename, false, "could not read file content")
+      }
+
+      // run the compare
+      var delta = diffPatcher.diff(jsonOld, jsonNew);
+      console.log("ℹ delta", delta)
+      //console.log(jsonDiffPatch.formatters.console.format(delta))
+
+
+      result = validateDiff(delta, simplePath)
+      setResult(filename, result.result, result.msg)
 
     }
+
+    printSummery()
+    if (summery.size != filesChanged) {
+      throw `Some files could not be classified, should be ${filesChanged} / was ${summery.size}`
+    }
+
+    console.log("All files could be classified ✔")
+    //check if map contains "false" elements
+    falseMap = new Map([...summery].filter(([k, v]) => v.result == false))
+    if (falseMap.size > 0) {
+      throw "PR contains changes that are not whitelisted"
+
+    }
+
+    console.log("all files seem to be valid and can be merged")
+
+
+
 
   } catch (error) {
     console.log("pipeline failed", error)
